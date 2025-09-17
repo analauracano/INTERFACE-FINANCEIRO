b24f6e29e5bbfefe0ebfd3403b7eb80de167d692
analauracano <laura.fioratti@outlook.com>
2025-09-17 20:58:05 +0100

    dashboard, transactions, card...

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,28 @@
-import { useEffect } from 'react';
-import { api } from '../services/api';
+import { useEffect, useState } from 'react';
+// import { api } from '../services/api';
 import { getAuth } from 'firebase/auth';
+import MonthYearSelect from '../components/MonthYearSelect';
+import { getTransactionsSummary } from '../services/transactionService';
+import type { TransactionSummary } from '../types/transactions';
+import Card from '../components/Card';
+import { ArrowUp, Wallet } from 'lucide-react'
+import { formatCurrency } from '../utils/formatter';
+
+const initialSummary: TransactionSummary = {
+    totalExpenses: 0,
+    totalIncomes: 0,
+    totalBalance: 0,
+    expensesByCategory: [],
+}
 
 const Dashboard = () => {
+    const currentDate = new Date();
+    const [year, setYear] = useState<number>(currentDate.getFullYear());
+    const [month, setMonth] = useState(currentDate.getMonth() + 1);
+    const [summary, setSummary] = useState<TransactionSummary>(initialSummary);
+
     useEffect(() => {
-        async function getTransactions() {
+        async function loadTransactionsSummary() {
             const auth = getAuth();
             const user = auth.currentUser;
 
@@ -14,23 +32,65 @@ const Dashboard = () => {
             }
 
             // Pega o token JWT do Firebase
-            const token = await user.getIdToken();
+            // const token = await user.getIdToken();
 
-            const response = await api.get('/transactions', {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
+            const response = await getTransactionsSummary(month, year);
 
-            console.log(response.data);
+            // const response = await api.get('/transactions', {
+            //     headers: {
+            //         Authorization: `Bearer ${token}`,
+            //     },
+            // });
+
+            setSummary(response);
         }
 
-        getTransactions();
-    }, []);
+        loadTransactionsSummary();
+    }, [month, year]);
 
     return (
-        <div>
-            <h1>Dashboard - Em Construção</h1>
+        <div className='container-app py-6'>
+            <div className='flex flex-col md:flex-row justify-between items-start md:items-center mb-6'>
+                <h1 className='text-2xl font-bold mb-4 md:mb-0'>Dashboard</h1>
+                <MonthYearSelect month={month} year={year} onMonthChange={setMonth} onYearChange={setYear} />
+            </div>
+
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+            <Card 
+            icon={<Wallet size={20}
+            className='text-primary-500'/>}
+            title='Saldo'
+            hover
+            glowEffect={summary.totalBalance > 0}>
+                <p className={`text-2xl font-semibold mt-2
+                ${summary.totalBalance > 0 ? 'text-primary-500' : 'text-red-600'}`}>
+                    {formatCurrency(summary.totalBalance)}
+                </p>
+            </Card>
+
+             <Card 
+            icon={<ArrowUp size={20}
+            className='text-primary-500'/>}
+            title='Receitas'
+            hover
+            >
+                <p className='text-2xl font-semibold mt-2 text-primary-500'
+            >
+                    {formatCurrency(summary.totalIncomes)}
+                </p>
+            </Card>
+
+             <Card 
+            icon={<Wallet size={20}
+            className='text-red-700'/>}
+            title='Despesas'
+            hover
+            >
+                <p className='text-2xl font-semibold mt-2 text-red-600'>
+                    {formatCurrency(summary.totalExpenses)}
+                </p>
+            </Card>
+            </div>
         </div>
     );
 };
---
+++ src/types/transactions.ts
@@ -0,0 +1,35 @@
+import type { Category, CategorySummary } from "./category";
+
+export const TransactionType = {
+    INCOME: 'income',
+    EXPENSE: 'expense'
+} as const;
+
+export type TransactionType = typeof TransactionType[keyof typeof TransactionType];
+
+export interface Transaction {
+    id: string;
+    description: string;
+    amount: number;
+    date: string | Date;
+    type: TransactionType;
+    categoryId: string;
+    category: Category;
+    updatedAt: string | Date;
+    createdAt: string | Date;
+    userId: string;
+}
+
+export interface TransactionFilter {
+    month: number;
+    year: number;
+    categoryId?: string;
+    type?: TransactionType;
+}
+
+export interface TransactionSummary {
+    totalExpenses: number;
+    totalIncomes: number;
+    totalBalance: number;
+    expensesByCategory: CategorySummary[];
+}
